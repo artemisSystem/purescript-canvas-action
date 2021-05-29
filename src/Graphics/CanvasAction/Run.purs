4477ddde1bd4818bd6ad7e65542d96ef54785876
artemisSystem <39034386+artemisSystem@users.noreply.github.com>
2021-05-29 13:32:47 +0200

    Update Run for 0.14, add CanvasAff functions

--- src/Graphics/CanvasAction/Run.purs
+++ src/Graphics/CanvasAction/Run.purs
@@ -2,75 +2,125 @@ module Graphics.CanvasAction.Run where
 
 import Prelude
 
+import Control.Monad.Reader (asks, runReaderT)
 import Data.Symbol (class IsSymbol)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff.Class as Aff
 import Effect.Class as Effect
 import Graphics.Canvas (Context2D)
-import Graphics.CanvasAction.Types (CanvasActionM, runAction)
+import Graphics.CanvasAction.Types (CanvasAction, CanvasAff)
 import Prim.Row as Row
-import Run (EFFECT, FProxy, Run, SProxy(..), case_, lift, on, runRec, interpret, send)
+import Run (AFF, EFFECT, Run, case_, lift, on, runRec)
+import Run.Reader (Reader(..), askAt, runReaderAt)
+import Type.Proxy (Proxy(..))
+import Type.Row (type (+))
 
--- | Type alias for using `CanvasActionM` as an effect
-type CANVAS = FProxy CanvasActionM
 
--- | Symbol proxy for "canvas"
-_canvas ∷ SProxy "canvas"
-_canvas = SProxy
+type CTX r = (ctx ∷ Reader Context2D | r)
 
--- | Lift a `CanvasActionM` into `Run` via the `canvas` label
-liftCanvasAction ∷ ∀ r a. CanvasActionM a → Run (canvas ∷ CANVAS | r) a
-liftCanvasAction = liftCanvasActionAt _canvas
+-- | Proxy for `"ctx"`
+_ctx ∷ Proxy "ctx"
+_ctx = Proxy
 
--- | Lift a `CanvasActionM` into `Run` via the provided label
+-- | Runs a `CTX` reader effect.
+runCtx ∷ ∀ r. Context2D → Run (CTX + r) ~> Run r
+runCtx = runReaderAt _ctx
+
+
+-- | Lift a `CanvasAction` into `Run` via the `ctx` and `effect` labels
+liftCanvasAction ∷ ∀ r. CanvasAction ~> Run (CTX + EFFECT + r)
+liftCanvasAction = liftCanvasActionAt _ctx (Proxy ∷ _ "effect")
+
+-- | Lift a `CanvasAction` into `Run` via the provided labels
 liftCanvasActionAt ∷
-  ∀ s r1 r2
-  . IsSymbol s ⇒ Row.Cons s CANVAS r1 r2
-  ⇒ SProxy s → CanvasActionM ~> Run r2
-liftCanvasActionAt = lift
+  ∀ proxy ctx effect ra rb r
+  . IsSymbol ctx
+  ⇒ IsSymbol effect
+  ⇒ Row.Cons ctx (Reader Context2D) ra r
+  ⇒ Row.Cons effect Effect rb r
+  ⇒ proxy ctx → proxy effect → CanvasAction ~> Run r
+liftCanvasActionAt ctxSym effectSym m = do
+  ctx ← askAt ctxSym
+  lift effectSym (runReaderT m ctx)
 
-runCanvas ∷
-  ∀ r
-  . Context2D
-  → Run (effect ∷ EFFECT, canvas ∷ CANVAS | r) ~> Run (effect ∷ EFFECT | r)
-runCanvas = runCanvasAt (SProxy ∷ _ "effect") _canvas
+-- | Runs `CTX` and `EFFECT` effects in `CanvasAction`
+runBaseCanvas ∷ Run (CTX + EFFECT + ()) ~> CanvasAction
+runBaseCanvas = runBaseCanvasAt _ctx (Proxy ∷ _ "effect")
 
-runCanvasAt ∷
-  ∀ effect canvas r0 r1 r2
-  . IsSymbol effect
-  ⇒ IsSymbol canvas
-  ⇒ Row.Cons effect EFFECT r0 r1
-  ⇒ Row.Cons canvas CANVAS r1 r2
-  ⇒ SProxy effect
-  → SProxy canvas
-  → Context2D
-  → Run r2 ~> Run r1
-runCanvasAt effect canvas ctx = interpret
-  (on canvas (runAction ctx >>> lift effect) send)
-
--- | Runs a base `CanvasActionM` effect
-runBaseCanvas ∷ Run (canvas ∷ CANVAS) ~> CanvasActionM
-runBaseCanvas = runBaseCanvasAt _canvas
-
--- | Runs a base `CanvasActionM` effect at the provided label
+-- | Runs `Reader Context2D` and `Effect` in `CanvasAction` at the provided
+-- | labels
 runBaseCanvasAt ∷
-  ∀ s r. IsSymbol s ⇒ Row.Cons s CANVAS () r ⇒ SProxy s → Run r ~> CanvasActionM
-runBaseCanvasAt p = runRec (case_ # on p identity)
+  ∀ proxy ctx effect r1 r2
+  . IsSymbol ctx
+  ⇒ IsSymbol effect
+  ⇒ Row.Cons ctx (Reader Context2D) () r1
+  ⇒ Row.Cons effect Effect r1 r2
+  ⇒ proxy ctx
+  → proxy effect
+  → Run r2 ~> CanvasAction
+runBaseCanvasAt ctxSym effectSym = case_
+  # on ctxSym (\(Reader f) → asks f)
+  # on effectSym Effect.liftEffect
+  # runRec
 
--- | Runs base `CanvasActionM` and `Effect` together as one effect
-runBaseCanvas' ∷ Run (effect ∷ EFFECT, canvas ∷ CANVAS) ~> CanvasActionM
-runBaseCanvas' = runBaseCanvasAt' (SProxy ∷ _ "effect") _canvas
 
--- | Runs base `CanvasActionM` and `Effect` together as one effect at the
--- | provided labels
-runBaseCanvasAt' ∷
-  ∀ effect canvas r1 r2
-  . IsSymbol effect
-  ⇒ IsSymbol canvas
-  ⇒ Row.Cons effect EFFECT () r1
-  ⇒ Row.Cons canvas CANVAS r1 r2
-  ⇒ SProxy effect
-  → SProxy canvas
-  → Run r2 ~> CanvasActionM
-runBaseCanvasAt' effect canvas = case_
-  # on effect Effect.liftEffect
-  # on canvas identity
+-- | Lift a `CanvasAff` into `Run` via the `ctx` and `aff` labels
+liftCanvasAff ∷ ∀ r. CanvasAff ~> Run (CTX + AFF + r)
+liftCanvasAff = liftCanvasAffAt _ctx (Proxy ∷ _ "aff")
+
+-- | Lift a `CanvasAff` into `Run` via the provided labels
+liftCanvasAffAt ∷
+  ∀ proxy ctx aff ra rb r
+  . IsSymbol ctx
+  ⇒ IsSymbol aff
+  ⇒ Row.Cons ctx (Reader Context2D) ra r
+  ⇒ Row.Cons aff Aff rb r
+  ⇒ proxy ctx → proxy aff → CanvasAff ~> Run r
+liftCanvasAffAt ctxSym affSym m = do
+  ctx ← askAt ctxSym
+  lift affSym (runReaderT m ctx)
+
+-- | Runs `CTX` and `AFF` effects in `CanvasAff`
+runBaseCanvasAff ∷ Run (CTX + AFF + ()) ~> CanvasAff
+runBaseCanvasAff = runBaseCanvasAffAt _ctx (Proxy ∷ _ "aff")
+
+-- | Runs `Reader Context2D` and `Aff` in `CanvasAff` at the provided labels
+runBaseCanvasAffAt ∷
+  ∀ proxy ctx aff r1 r2
+  . IsSymbol ctx
+  ⇒ IsSymbol aff
+  ⇒ Row.Cons ctx (Reader Context2D) () r1
+  ⇒ Row.Cons aff Aff r1 r2
+  ⇒ proxy ctx
+  → proxy aff
+  → Run r2 ~> CanvasAff
+runBaseCanvasAffAt ctxSym affSym = case_
+  # on ctxSym (\(Reader f) → asks f)
+  # on affSym Aff.liftAff
+  # runRec
+
+-- | Runs `CTX`, `EFFECT`, and `AFF` effects in `CanvasAff`
+runBaseCanvasAff' ∷ Run (CTX + EFFECT + AFF + ()) ~> CanvasAff
+runBaseCanvasAff' =
+  runBaseCanvasAffAt' _ctx (Proxy ∷ _ "effect") (Proxy ∷ _ "aff")
+
+-- | Runs `Reader Context2D`, `Effect`, and `Aff` in `CanvasAff` at the provided
+-- | labels
+runBaseCanvasAffAt' ∷
+  ∀ proxy ctx effect aff r1 r2 r3
+  . IsSymbol ctx
+  ⇒ IsSymbol effect
+  ⇒ IsSymbol aff
+  ⇒ Row.Cons ctx (Reader Context2D) () r1
+  ⇒ Row.Cons effect Effect r1 r2
+  ⇒ Row.Cons aff Aff r2 r3
+  ⇒ proxy ctx
+  → proxy effect
+  → proxy aff
+  → Run r3 ~> CanvasAff
+runBaseCanvasAffAt' ctxSym effectSym affSym = case_
+  # on ctxSym (\(Reader f) → asks f)
+  # on effectSym Effect.liftEffect
+  # on affSym Aff.liftAff
   # runRec
